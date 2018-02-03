895ac3706d814fc244037619a092842de68686c9
Jorge Aznar <jorgeatgu@gmail.com>
2018-02-03 16:15:05 +0100

    sobrecullidas Zaragoza y alcanyiz

---
+++ data/fogache.js
@@ -0,0 +1,661 @@
+//Forman un fuego todas aquellas personas que habitan
+//en una casa y tomen la despensa de un superior o pater familias
+//continuamente en dicha casa, lo que ha de jurar el superior o pater familiar
+//Cortes de Tarazona de 1495
+
+var ofogache = [{
+        "nombre": "Alfocea",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 26
+    },
+    {
+        "nombre": "Julisbol",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 42
+    },
+    {
+        "nombre": "Villanueva de Gállego",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 47
+    },
+    {
+        "nombre": "Zuera",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 151
+    },
+    {
+        "nombre": "San Mateo de Gállego",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 49
+    },
+    {
+        "nombre": "Leciñena",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 59
+    },
+    {
+        "nombre": "Perdiguera",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 61
+    },
+    {
+        "nombre": "Peñaflor",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 52
+    },
+    {
+        "nombre": "Pastriz",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 41
+    },
+    {
+        "nombre": "Movera",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 1
+    },
+    {
+        "nombre": "Puebla de Alfinden",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 47
+    },
+    {
+        "nombre": "Alfajarin",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 55
+    },
+    {
+        "nombre": "Nuez de Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 54
+    },
+    {
+        "nombre": "Villafranca de Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 37
+    },
+    {
+        "nombre": "Osera",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 39
+    },
+    {
+        "nombre": "Aguilar de Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 5
+    },
+    {
+        "nombre": "Pina de Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 157
+    },
+    {
+        "nombre": "Gelsa",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 90
+    },
+    {
+        "nombre": "Velilla de Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 28
+    },
+    {
+        "nombre": "Alforque",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 17
+    },
+    {
+        "nombre": "Alborge",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 51
+    },
+    {
+        "nombre": "Cinco Olivas",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 12
+    },
+    {
+        "nombre": "Sastago",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 76
+    },
+    {
+        "nombre": "Escatron",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 88
+    },
+    {
+        "nombre": "Valimaña",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 1
+    },
+    {
+        "nombre": "Chiprana",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 24
+    },
+    {
+        "nombre": "Maella",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 196
+    },
+    {
+        "nombre": "Calaceite",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 114
+    },
+    {
+        "nombre": "Nonaspe",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 86
+    },
+    {
+        "nombre": "Fabara",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 129
+    },
+    {
+        "nombre": "Caspe",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 295
+    },
+    {
+        "nombre": "Castelnou",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 13
+    },
+    {
+        "nombre": "Jatiel",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 17
+    },
+    {
+        "nombre": "Samper de Calanda",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 129
+    },
+    {
+        "nombre": "La Puebla de Híjar",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 69
+    },
+    {
+        "nombre": "Híjar",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 211
+    },
+    {
+        "nombre": "Urrea de Gaen",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 52
+    },
+    {
+        "nombre": "Albalate del Arzobispo",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 238
+    },
+    {
+        "nombre": "Ariño",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 21
+    },
+    {
+        "nombre": "Oliete",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 86
+    },
+    {
+        "nombre": "Alcaine",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 25
+    },
+    {
+        "nombre": "Alacón",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 35
+    },
+    {
+        "nombre": "Vinaceite",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 32
+    },
+    {
+        "nombre": "Azaila",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 12
+    },
+    {
+        "nombre": "Pardina Monasterio de Rueda",
+        "sobrecullida": "Zaragoza",
+        "poblacion": "despoblado"
+    },
+    {
+        "nombre": "La Zaida",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 29
+    },
+    {
+        "nombre": "Quinto",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 77
+    },
+    {
+        "nombre": "Fuentes de Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 197
+    },
+    {
+        "nombre": "Roden",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 47
+    },
+    {
+        "nombre": "Mediana",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 106
+    },
+    {
+        "nombre": "Valmadrid",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 20
+    },
+    {
+        "nombre": "Torrecilla Valmadrid",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 12
+    },
+    {
+        "nombre": "El burgo del Ebro",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 13
+    },
+    {
+        "nombre": "Cuarte de Huerva",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 82
+    },
+    {
+        "nombre": "Cadrete",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 80
+    },
+    {
+        "nombre": "Jaulín",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 7
+    },
+    {
+        "nombre": "Botorrita",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 20
+    },
+    {
+        "nombre": "La Muela",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 22
+    },
+    {
+        "nombre": "Mozota",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 38
+    },
+    {
+        "nombre": "Muel",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 99
+    },
+    {
+        "nombre": "Mezalocha",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 35
+    },
+    {
+        "nombre": "Longares",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 117
+    },
+    {
+        "nombre": "Ayles",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 1
+    },
+    {
+        "nombre": "Villanueva del Huerva",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 84
+    },
+    {
+        "nombre": "Tosos",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 38
+    },
+    {
+        "nombre": "Aguilon",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 84
+    },
+    {
+        "nombre": "Fuendetodos",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 64
+    },
+    {
+        "nombre": "La puebla de Albortón",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 30
+    },
+    {
+        "nombre": "Almonacid de la Cuba",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 25
+    },
+    {
+        "nombre": "Moneva",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 42
+    },
+    {
+        "nombre": "Samper del Salz",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 15
+    },
+    {
+        "nombre": "Lagata",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 50
+    },
+    {
+        "nombre": "Letux",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 53
+    },
+    {
+        "nombre": "Lecera",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 85
+    },
+    {
+        "nombre": "Belchite",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 245
+    },
+    {
+        "nombre": "Codo",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 28
+    },
+    {
+        "nombre": "Villamayor",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 80
+    },
+    {
+        "nombre": "María de Huerva",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 91
+    },
+    {
+        "nombre": "Zaragoza",
+        "sobrecullida": "Zaragoza",
+        "poblacion": 3983
+    },
+    {
+        "nombre": "Ricla",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 83
+    },
+    {
+        "nombre": "La Almunia de Doña Godina",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 185
+    },
+    {
+        "nombre": "Alpartir",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 45
+    },
+    {
+        "nombre": "Almonacid de la Sierra",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 88
+    },
+    {
+        "nombre": "Obón",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 64
+    },
+    {
+        "nombre": "Estercuel",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 79
+    },
+    {
+        "nombre": "Cañizar del Olivar",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 28
+    },
+    {
+        "nombre": "La Coma",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 5
+    },
+    {
+        "nombre": "Ejulve",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 124
+    },
+    {
+        "nombre": "Montoro",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 17
+    },
+    {
+        "nombre": "Las Cuevas de Cañart",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 116
+    },
+    {
+        "nombre": "Ladruñan",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 28
+    },
+    {
+        "nombre": "Bordon",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 38
+    },
+    {
+        "nombre": "Luquo",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 32
+    },
+    {
+        "nombre": "Castellote",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 85
+    },
+    {
+        "nombre": "Molinos",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 166
+    },
+    {
+        "nombre": "Verge",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 21
+    },
+    {
+        "nombre": "Los Olmos",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 28
+    },
+    {
+        "nombre": "La Mata de los Olmos",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 51
+    },
+    {
+        "nombre": "Crivillén",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 76
+    },
+    {
+        "nombre": "Alloza",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 114
+    },
+    {
+        "nombre": "Andorra",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 136
+    },
+    {
+        "nombre": "Alcorisa",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 176
+    },
+    {
+        "nombre": "Foz-Calanda",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 35
+    },
+    {
+        "nombre": "La Ginebrosa",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 58
+    },
+    {
+        "nombre": "Aguaviva",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 44
+    },
+    {
+        "nombre": "Monroyo",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 105
+    },
+    {
+        "nombre": "Cerollera",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 16
+    },
+    {
+        "nombre": "Torre de Arcas",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 20
+    },
+    {
+        "nombre": "Peñarroya de Tastavins",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 93
+    },
+    {
+        "nombre": "Fuentespalda",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 60
+    },
+    {
+        "nombre": "Beceite",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 108
+    },
+    {
+        "nombre": "Valderrobres",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 135
+    },
+    {
+        "nombre": "la Fresneda",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 180
+    },
+    {
+        "nombre": "Torre del Compte",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 49
+    },
+    {
+        "nombre": "Cretas",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 91
+    },
+    {
+        "nombre": "Lledó",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 13
+    },
+    {
+        "nombre": "Arens de Lledó",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 35
+    },
+    {
+        "nombre": "Mazaleón",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 43
+    },
+    {
+        "nombre": "Valdeltormo",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 36
+    },
+    {
+        "nombre": "Valjunquera",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 54
+    },
+    {
+        "nombre": "Fornoles",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 41
+    },
+    {
+        "nombre": "Rafales",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 57
+    },
+    {
+        "nombre": "Belmonte de Mezquín",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 47
+    },
+    {
+        "nombre": "La Codoñera",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 102
+    },
+    {
+        "nombre": "Castelseras",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 44
+    },
+    {
+        "nombre": "Torrecilla de Alcañiz",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 35
+    },
+    {
+        "nombre": "Valdealgorfa",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 75
+    },
+    {
+        "nombre": "Alcañiz",
+        "sobrecullida": "Alcañiz",
+        "poblacion": 705
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
