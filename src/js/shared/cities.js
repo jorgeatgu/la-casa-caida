325483510fe7e63df62b6143c33060f0f49327ff
jorgeatgu <jorgeatgu@gmail.com>
2025-03-21 16:35:10 +0100

    paths

--- src/js/shared/cities.js
+++ src/js/shared/cities.js
@@ -5,9 +5,9 @@ export const cities = [
     lineTotalCSV: 'data/huesca/huesca-total.csv',
     scatterUnderCSV: 'data/huesca/huesca-mayor-menor.csv',
     vegetativeCSV: 'data/huesca/saldo-vegetativo-total-huesca.csv',
-    comparatorCSV: 'data/evolucion/huesca/huesca-total.csv',
+    comparatorCSV: 'data/huesca/huesca-evolucion.csv',
     evolutionCSV: 'data/huesca/huesca-2010-2020.csv',
-    groupByAgeCSV: 'data/data2020/huesca-years-groups-total.csv',
+    groupByAgeCSV: 'data/huesca/huesca-years-groups-total.csv',
     densityCSV: 'data/huesca/huesca-densidad.csv'
   },
   {
@@ -16,9 +16,9 @@ export const cities = [
     lineTotalCSV: 'data/teruel/teruel-total.csv',
     scatterUnderCSV: 'data/teruel/teruel-mayor-menor.csv',
     vegetativeCSV: 'data/teruel/saldo-vegetativo-total-teruel.csv',
-    comparatorCSV: 'data/evolucion/teruel/teruel-total.csv',
+    comparatorCSV: 'data/teruel/teruel-evolucion.csv',
     evolutionCSV: 'data/teruel/teruel-2010-2020.csv',
-    groupByAgeCSV: 'data/data2020/teruel-years-groups-total.csv',
+    groupByAgeCSV: 'data/teruel/teruel-years-groups-total.csv',
     densityCSV: 'data/teruel/teruel-densidad.csv'
   },
   {
@@ -27,9 +27,9 @@ export const cities = [
     lineTotalCSV: 'data/zaragoza/zaragoza-total.csv',
     scatterUnderCSV: 'data/zaragoza/zaragoza-mayor-menor.csv',
     vegetativeCSV: 'data/zaragoza/saldo-vegetativo-total-zaragoza.csv',
-    comparatorCSV: 'data/evolucion/zaragoza/zaragoza-total.csv',
+    comparatorCSV: 'data/zaragoza/zaragoza-evolucion.csv',
     evolutionCSV: 'data/zaragoza/zaragoza-2010-2020.csv',
-    groupByAgeCSV: 'data/data2020/zaragoza-years-groups-total.csv',
+    groupByAgeCSV: 'data/zaragoza/zaragoza-years-groups-total.csv',
     densityCSV: 'data/zaragoza/zaragoza-densidad.csv'
   },
 ];
